66ba7f40bb837d21a95690e83de2b41dc2fe0868
Olivia Brode-Roger <nibr@mit.edu>
2020-05-13 13:25:18 -0400

    Forgot a file..

---
+++ src/tcp.rs
@@ -0,0 +1,58 @@
+
+#[derive(Debug)]
+pub struct Packet {
+    pub src: usize,
+    pub dst: usize,
+    pub seq_num: u64,
+    pub size_byte: u64,
+
+    pub ttl: u64,
+    pub sent_ns: u64,
+}
+
+#[derive(Debug)]
+pub struct Flow {
+    pub src: usize,
+    pub dst: usize,
+    pub size_byte: u64,
+
+    cwnd: u64,
+    next_seq: u64,
+}
+
+const BYTES_PER_PACKET: u64 = 1500;
+
+impl Flow {
+    pub fn new(src: usize, dst: usize, n_packets: u64) -> Flow {
+        Flow {
+            src,
+            dst,
+
+            size_byte: n_packets * BYTES_PER_PACKET,
+            cwnd: 1,
+            next_seq: 0,
+        }
+    }
+}
+
+impl Iterator for Flow {
+    type Item = Packet;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_seq * BYTES_PER_PACKET < self.size_byte {
+            let p = Packet {
+                src: self.src,
+                dst: self.dst,
+                seq_num: self.next_seq,
+                size_byte: BYTES_PER_PACKET,
+                ttl: 10,
+                sent_ns: 0,
+            };
+            self.next_seq += 1;
+            Some(p)
+        } else {
+            None
+        }
+    }
+}
+
