ee8a6a2e5f27aa442c6c909f99f1f2a93da4e021
Olivia Brode-Roger <nibr@mit.edu>
2020-06-22 23:38:15 -0400

    less trivial example

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     //                      s  ms  us  ns
     //let time_limit: u64 = 000_111_111_000;
 
-    let n_racks = 2;
+    let n_racks = 3;
 
     println!("Setup...");
     let world = World::new(n_racks);
