18357698a1cbbadf9a5359e3d202decd582e6f8f
Olivia Brode-Roger <nibr@mit.edu>
2020-07-22 17:15:03 -0400

    explanatory comments

--- rustasim-models/src/lib.rs
+++ rustasim-models/src/lib.rs
@@ -1,9 +1,11 @@
+// I like to have many small files
 mod network;
 mod router;
 mod routing;
 mod server;
 mod tcp;
 
+// but it's much easier to use if they're not in different modules
 pub use self::network::*;
 pub use self::router::*;
 pub use self::routing::*;
